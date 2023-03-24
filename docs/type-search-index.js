4ce8d548dd1f6359a44b97857eb88fb9987a0ca7
devmanso <itsnothisuser@gmail.com>
2023-03-23 20:57:02 -0400

    remove docs

--- docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.hyperdrive.hyperlib","l":"DriveFilter"},{"p":"com.hyperdrive.hyperlib","l":"LimeLight"}];updateSearchResults();
